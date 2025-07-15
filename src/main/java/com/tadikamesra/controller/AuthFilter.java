e2bc0529b8c39948adfc9ff6d61344e250b4ccc6
hans129 <irfanrdh129@gmail.com>
2025-07-15 21:08:54 +0700

    membuat ulang struktur project

--- src/main/java/com/tadikamesra/controller/AuthFilter.java
+++ src/main/java/com/tadikamesra/controller/AuthFilter.java
@@ -5,9 +5,20 @@ import javax.servlet.http.*;
 import java.io.IOException;
 
 public class AuthFilter implements Filter {
+    @Override
     public void init(FilterConfig config) {}
+    @Override
     public void destroy() {}
 
+    /**
+     *
+     * @param request
+     * @param response
+     * @param chain
+     * @throws IOException
+     * @throws ServletException
+     */
+    @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
 
